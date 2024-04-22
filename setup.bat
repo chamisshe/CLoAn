8acd5c3e561ee4b945a36d3315038f806bd4480b
notmyyka <kolleghessig@gmail.com>
2024-04-22 17:47:06 +0200

    added utoken-fixes in setup-scripts

--- setup.bat
+++ setup.bat
@@ -4,6 +4,7 @@ python -m venv .venv-cloan
 
 pip install -r requirements.txt
 
+((Get-Content -path ".4-venv-cloan\Lib\site-packages\utoken\util.py" -Raw).Replace("with open(filename)", "with open(filename, encoding='utf-8')") | Set-Content -Path ".4-venv-cloan\Lib\site-packages\utoken\util.py")
 echo -e ".venv-cloan\nsetup.bat" > .gitignore
 mkdir "data\marking"
 mkdir "data\output"
